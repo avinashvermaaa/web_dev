98f9b1695aad3726a16a88c6cd4e4e7f1bf50362
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-06-17 03:21:43 +0530

    Create days_weeks_months_left.js

---
+++ 14/days_weeks_months_left.js
@@ -0,0 +1,18 @@
+// function lifeInWeeks(age)
+ {
+    var age = 23;
+    age = 55 - age;
+    var days;
+    var weeks;
+    var months;
+
+  days = 365 * age;
+  weeks = 52 * age;
+  months = 12 * age;
+
+
+  console.log("You have "+ days + " days, " + weeks +" weeks, and " + months+" months left.");
+
+
+}
+
