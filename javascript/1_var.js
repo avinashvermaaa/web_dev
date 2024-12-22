d83ce6379690830c628fc95f50f5e6738f574bc6
Avinash Kumar <code6969nation@gmail.com>
2024-12-22 20:20:39 +0530

    javascript folder update
    update version 0.01

---
+++ javascript/1_var.js
@@ -0,0 +1,20 @@
+console.log("hello")
+console.log(2+3+"abc"+" "+"ok")
+// console.log("\n")
+
+if(NaN == NaN) console.log("yes")
+else console.log("no")
+
+var a = 8
+var b = 5
+
+console.log("previous a = " + a)
+console.log("previous b = " + b)
+
+a += b
+b = a - b
+a = a -b
+
+
+console.log("new a = " + a)
+console.log("new b = " + b)
---
+++ javascript/3_bmi.js
@@ -0,0 +1,9 @@
+console.log("gadf")
+var lowda = 6.49999
+var rounda = Math.round(lowda)
+var uppr = Math.ceil(lowda)
+var lowr = Math.floor(lowda)
+
+console.log(rounda)
+console.log(uppr)
+console.log(lowr)
