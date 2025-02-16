2600f6a9f1d3e0e3d7a2821dcbe67c98e13b951d
Avinash <code6969nation@gmail.com>
2025-02-16 21:45:58 +0530

    React_todo app

---
+++ A_Fullstack/React_js/day1/todo-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> hello this is a todo list app</h1>
+    </div>
+  );
+}
+
+export default App;
