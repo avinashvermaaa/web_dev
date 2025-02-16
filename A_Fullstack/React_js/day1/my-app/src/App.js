6c71bd7b15061658b0b2106c92a862ab82189a28
Avinash <code6969nation@gmail.com>
2025-02-16 22:06:23 +0530

    React_day1_my-app_welcome.js
    reusable component is created and used in app.js to pass parameter to welcome.js and get exports {output} on site.

--- A_Fullstack/React_js/day1/my-app/src/App.js
+++ A_Fullstack/React_js/day1/my-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+import React from 'react';
+import Welcome from './Welcome.js';
+
 function App() {
   return (
     <div>
       <h1>Hello Reactjs!</h1>
+      <Welcome name= "Avinash" />
     </div>
   );
 }
---
+++ A_Fullstack/React_js/day1/my-app/src/Welcome.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function welcome(props){
+    return <h2>kaisa hai,{props.name}, Bhai! </h2>
+}
+
+export default welcome;
